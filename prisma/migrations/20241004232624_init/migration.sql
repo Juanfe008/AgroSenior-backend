a3ca8df7cc73688ead92229c2ad57b7c305108f6
Juanfe008 <54733201+Juanfe008@users.noreply.github.com>
2025-01-16 17:37:02 -0500

    Migraciones para: Actividades y Huerto

--- prisma/migrations/20241004232624_init/migration.sql
+++ prisma/migrations/20241004232624_init/migration.sql
@@ -43,6 +43,7 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,          
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "exp" INTEGER NOT NULL DEFAULT 0,
+    "nivel" INTEGER NOT NULL DEFAULT 1,
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
@@ -75,7 +76,7 @@ CREATE TABLE "CuestionarioCompletado" (
 -- CreateTable para Post
 CREATE TABLE "Post" (
     "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
+    "title" TEXT,
     "content" TEXT NOT NULL,
     "imageUrl" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -92,6 +93,72 @@ CREATE TABLE "PostLike" (
     CONSTRAINT "PostLike_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable para Actividad
+CREATE TABLE "Actividad" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "desc" TEXT NOT NULL,
+    "nivelMin" INTEGER NOT NULL,
+    "exp" INTEGER NOT NULL DEFAULT 10,
+    "tipo" TEXT NOT NULL DEFAULT 'virtual',
+    CONSTRAINT "Actividad_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable para ActividadCompletada
+CREATE TABLE "ActividadCompletada" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "actividadId" INTEGER NOT NULL,
+    "completadoEn" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "ActividadCompletada_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Huerto" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "light" INTEGER NOT NULL DEFAULT 50,
+    "water" INTEGER NOT NULL DEFAULT 50,
+    "nutrients" INTEGER NOT NULL DEFAULT 50,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Huerto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PlantType" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "light" INTEGER NOT NULL,
+    "water" INTEGER NOT NULL,
+    "nutrients" INTEGER NOT NULL,
+    "images" TEXT[],
+
+    CONSTRAINT "PlantType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateUniqueIndex
+CREATE UNIQUE INDEX "PlantType_name_key" ON "PlantType"("name");
+
+-- CreateTable
+CREATE TABLE "Plant" (
+    "id" SERIAL NOT NULL,
+    "huertoId" INTEGER NOT NULL,
+    "plantTypeId" INTEGER NOT NULL,
+    "growthStage" INTEGER NOT NULL DEFAULT 0,
+    "positionX" INTEGER NOT NULL,
+    "positionY" INTEGER NOT NULL,
+    "plantHealth" INTEGER NOT NULL DEFAULT 100,
+    "plantedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastWatered" TIMESTAMP(3),
+    "lastHarvested" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Plant_pkey" PRIMARY KEY ("id")
+);
+
 -- Índices y Claves Foráneas
 
 -- Índices únicos para User
@@ -106,6 +173,12 @@ CREATE UNIQUE INDEX "CuestionarioCompletado_userId_cuestionarioId_key" ON "Cuest
 
 -- Índice único para PostLike
 CREATE UNIQUE INDEX "PostLike_postId_userId_key" ON "PostLike"("postId", "userId");
+
+-- Índice único para ActividadCompletada
+CREATE UNIQUE INDEX "ActividadCompletada_userId_actividadId_key" ON "ActividadCompletada"("userId", "actividadId");
+
+-- Índice único para Huerto
+CREATE UNIQUE INDEX "Huerto_userId_key" ON "Huerto"("userId");
 
 -- Foreign Key para Leccion -> Nivel y Cuestionario
 ALTER TABLE "Leccion" 
@@ -142,3 +215,27 @@ ALTER TABLE "PostLike"
     ADD CONSTRAINT "PostLike_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE "PostLike" 
     ADD CONSTRAINT "PostLike_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- Foreign Key para ActividadCompletada -> User y Actividad
+ALTER TABLE "ActividadCompletada" 
+    ADD CONSTRAINT "ActividadCompletada_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ActividadCompletada" 
+    ADD CONSTRAINT "ActividadCompletada_actividadId_fkey" FOREIGN KEY ("actividadId") REFERENCES "Actividad"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Huerto" ADD CONSTRAINT "Huerto_userId_fkey" 
+FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Plant" ADD CONSTRAINT "Plant_huertoId_fkey" 
+FOREIGN KEY ("huertoId") REFERENCES "Huerto"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Plant" ADD CONSTRAINT "Plant_plantTypeId_fkey" 
+FOREIGN KEY ("plantTypeId") REFERENCES "PlantType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- Insertar los datos iniciales en la tabla PlantType
+INSERT INTO "PlantType" ("name", "light", "water", "nutrients", "images") VALUES
+('Planta 1', 50, 50, 50, ARRAY['/images/huerto/maceta-semilla.png', '/images/huerto/maceta-semilla.png', '/images/huerto/Maceta-planta-1.png']),
+('Planta 2', 50, 80, 60, ARRAY['/images/huerto/maceta-semilla.png', '/images/huerto/maceta-semilla.png', '/images/huerto/Maceta-planta-2.png']),
+('Planta 3', 70, 60, 70, ARRAY['/images/huerto/maceta-semilla.png', '/images/huerto/maceta-semilla.png', '/images/huerto/Maceta-planta-3.png']);
