ff45327f0db8c75ff131b8967a16769099a9c751
Juanfe008 <54733201+Juanfe008@users.noreply.github.com>
2025-01-16 19:50:16 -0500

    setup Cloudinary

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, FileTypeValidator, Get, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CloudinaryService } from './cloudinary/cloudinary.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly cloudinaryService: CloudinaryService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 4}),
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+        ]
+      })
+    ) file: Express.Multer.File) {
+    
+    return this.cloudinaryService.uploadFile(file)
+  }
+
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from 'prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { LeccionModule } from './leccion/leccion.module';
@@ -9,9 +10,10 @@ import { CuestionarioModule } from './cuestionario/cuestionario.module';
 import { ForoModule } from './foro/foro.module';
 import { ActividadesModule } from './actividades/actividades.module';
 import { HuertoModule } from './huerto/huerto.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, LeccionModule, CuestionarioModule, ForoModule, ActividadesModule, HuertoModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, LeccionModule, CuestionarioModule, ForoModule, ActividadesModule, HuertoModule, CloudinaryModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService]
